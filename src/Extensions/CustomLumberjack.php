9f47b0e7bd5366a2496e784fbf5cffd8917f449d
Itayi Patrick Chito-voro <patrick@chitosystems.com>
2020-12-21 16:26:36 +0200

    namespaced templates

---
+++ src/Extensions/CustomLumberjack.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Restruct\Essentials\Extensions {
+
+    use SilverStripe\CMS\Controllers\CMSPagesController;
+    use SilverStripe\Control\Controller;
+    use SilverStripe\Lumberjack\Model\Lumberjack;
+
+    class CustomLumberjack extends Lumberjack
+    {
+
+        /**
+         * Checks if we're on a controller where we should filter. ie. Are we loading the SiteTree?
+         *
+         * @return bool
+         */
+        protected function shouldFilter()
+        {
+            $controller = Controller::curr();
+
+            return get_class($controller) === CMSPagesController::class
+                // DON'T filter listview, after all, that's what its for (to show large sets of pages)
+                && in_array($controller->getAction(), [ "treeview", "getsubtree" ]);
+        }
+
+    }
+}
